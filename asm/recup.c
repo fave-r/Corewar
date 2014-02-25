3db6bf54b2ee3975a2942bd3263118d38c0d7cdd
odet_a <odet_a@epitech.eu>
2014-02-25 17:30:38 +0100

    init

---
+++ asm/recup.c
@@ -0,0 +1,16 @@
+/*
+** recup.c for recup in /home/odet_a/rendu/CPE_2014_corewar/asm
+** 
+** Made by 
+** Login   <odet_a@epitech.net>
+** 
+** Started on  Tue Feb 25 17:20:32 2014 
+** Last update Tue Feb 25 17:24:50 2014 
+*/
+
+#include "struct.h"
+
+char	*recup_name(char *str)
+{
+  
+}
