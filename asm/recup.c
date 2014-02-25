6addfc9d6daf0c9267b22a683eb2a0418d59e774
fave-r <romaric.fave@epitech.eu>
2014-02-25 19:51:48 +0100

    recup name, good Makefile

--- asm/recup.c
+++ asm/recup.c
@@ -1,16 +1,23 @@
 /*
-** recup.c for recup in /home/odet_a/rendu/CPE_2014_corewar/asm
-** 
-** Made by 
-** Login   <odet_a@epitech.net>
-** 
-** Started on  Tue Feb 25 17:20:32 2014 
-** Last update Tue Feb 25 17:24:50 2014 
+** recup.c for Corewar in /home/blackbird/work/Corewar/asm
+**
+** Made by romaric
+** Login   <fave_r@epitech.net>
+**
+** Started on  Tue Feb 25 18:56:54 2014 romaric
+** Last update Tue Feb 25 19:42:59 2014 romaric
 */
 
 #include "struct.h"
 
 char	*recup_name(char *str)
 {
-  
+  int	i;
+
+  i = 6;
+  while (str[++i] != '"' && count_quotes(str, i) >= 1)
+    {
+      write(1, &str[i], 1);
+    }
+  return ("BG");
 }
