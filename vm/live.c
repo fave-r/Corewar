3420912b43f923a2821bc3da6dca4c5199d42b4f
denche_t <denche_t@epitech.eu>
2014-03-05 16:45:00 +0100

    test

---
+++ vm/live.c
@@ -0,0 +1,18 @@
+/*
+** live.c for vm in /home/thibaud/rendu/Corewar/Corewar/vm
+** 
+** Made by thibaud
+** Login   <thibaud@epitech.net>
+** 
+** Started on  Wed Mar  5 00:04:42 2014 thibaud
+** Last update Wed Mar  5 00:53:45 2014 thibaud
+*/
+
+#include "vm.h"
+
+int	live(t_champ *champ, t_cor *cor)
+{
+  t_champ	*cur;
+  cur = cor->champ;
+  return (0);
+}
