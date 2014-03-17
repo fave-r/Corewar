a581e4e89f473c51654098016f9350dd2fb076e3
lopez_t <lopez_t@epitech.eu>
2014-03-17 14:17:08 +0100

    gsdgsd

--- vm/no_encode.c
+++ vm/no_encode.c
@@ -5,7 +5,7 @@
 ** Login   <thibaut.lopez@epitech.net>
 ** 
 ** Started on  Wed Feb 26 12:05:37 2014 Thibaut Lopez
-** Last update Mon Mar 17 14:13:51 2014 Thibaut Lopez
+** Last update Mon Mar 17 14:16:47 2014 Thibaut Lopez
 */
 
 #include "my.h"
@@ -14,39 +14,13 @@
 int	my_live(t_champ *champ, t_cor *cor)
 {
   int	live;
-  int	i;
-  int	j;
-  int	direct_arg;
-  int	**encode;
 
-  encode = get_encode(cor->mem, champ->pc);
-
-  i = 0;
-  j = -1;
-  while (i++ < 4)
-    {
-      if (cor->champ->nb[i - 1] == direct_arg)
-	j = i - 1;
-    }
-  if (j != -1)
-    {
-      if (j != -1)
-	{
-	  my_putstr("live du champion : ", 1);
-	  my_putstr(champ->head->prog_name, 1);
-	  my_putstr(", pour le numéro : ", 1);
-	  my_putnbr(, 1);
-	  cor->live_done++;
-	  i = 0;
-	  while (i++ < 4)
-	    if (cor->live[i - 1] == 2)
-	      cor->live[i - 1] = 1;
-	  cor->live[j] = 2;
-	}
-    }
-
+  my_putstr("live du champion : ", 1);
+  my_putstr(champ->head->prog_name, 1);
+  my_putstr(", pour le numéro : ", 1);
+  live = get_nbr_action(cor->mem, champ->pc, 4);
+  my_putnbr(live, 1);
   my_putstr(", avance dans la mémoire de 4\n", 1);
-  champ->pc += 6;
   return (5);
 }
 
