1bc6548c1fc91866279763caadcb2c5aa9be24ce
lopez_t <lopez_t@epitech.eu>
2014-03-18 20:13:58 +0100

    Lancement de la lib graphique

--- vm/main.c
+++ vm/main.c
@@ -5,7 +5,7 @@
 ** Login   <thibaud@epitech.net>
 ** 
 ** Started on  Tue Feb 25 15:57:49 2014 thibaud
-** Last update Tue Mar 18 19:53:10 2014 Thibaut Lopez
+** Last update Tue Mar 18 20:13:40 2014 Thibaut Lopez
 */
 
 #include "my.h"
@@ -143,7 +143,7 @@ int	main(int argc, char **argv)
   cor.nb_chmps_alive = 4;
   my_bzero(cor.live, 4);
   init_champs_nb(&cor);
-  init_graphic(&cor);
+  //init_graphic(&cor); c'est pour l'aperçu graphique, ne le lance pas maintenant, j'ai pas fini
   /*
   int	i;
 
