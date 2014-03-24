430be99e3f99dc3f6637ba678c8f1de21c39c5d1
lopez_t <lopez_t@epitech.eu>
2014-03-24 14:42:46 +0100

    Mise à la norme + fork et lfork

---
+++ vm/Function/other_fun.c
@@ -0,0 +1,65 @@
+/*
+** other_fun.c for other_fun in /home/thibaut.lopez/Corewar/Corewar/vm
+** 
+** Made by Thibaut Lopez
+** Login   <thibaut.lopez@epitech.net>
+** 
+** Started on  Wed Mar 12 19:20:50 2014 Thibaut Lopez
+** Last update Mon Mar 24 14:15:54 2014 Thibaut Lopez
+*/
+
+#include "vm.h"
+#include "my.h"
+
+int	my_st(t_champ *champ, t_cor *cor)
+{
+  int	**tab;
+
+  tab = get_encode(cor->mem, champ->pc);
+  if ((tab[0][0] == 1 && check_reg(tab[0][2]))
+      && ((tab[1][0] == 1 && check_reg(tab[1][2])) || tab[1][0] == 3))
+    {
+      if (tab[1][0] == 1)
+	champ->reg[tab[1][2]] = champ->reg[tab[0][2]];
+      else
+	print_on_mem(cor, champ->reg[tab[0][2] - 1], champ->pc + tab[1][2]);
+      my_printf(1, "ST du champion %s\n", champ->head->prog_name);
+    }
+  else
+    my_putstr("ST FAIL\n", 1);
+  champ->pc += tab[0][1] + tab[1][1] + 2;
+  return (tab[0][1] + tab[1][1] + 2);
+}
+
+int	my_sti(t_champ *champ, t_cor *cor)
+{
+  int	**tab;
+
+  tab = get_encode(cor->mem, champ->pc);
+  if (tab[0][0] == 1 && check_reg(tab[0][2])
+      && (tab[1][0] > 1 || (tab[1][0] == 1 && check_reg(tab[1][2])))
+      && ((tab[2][0] == 1 && check_reg(tab[2][2]))|| tab[2][0] == 2))
+    {
+      print_on_mem(cor, champ->reg[tab[0][2] - 1],
+		   champ->pc + tab[1][2] + tab[2][2]);
+      my_printf(1, "STI du champion %s\n", champ->head->prog_name);
+    }
+  else
+    my_putstr("STI FAIL\n", 1);
+  champ->pc += tab[0][1] + tab[1][1] + tab[2][1] + 2;
+  return (tab[0][1] + tab[1][1] + tab[2][1] + 2);
+}
+
+int	my_aff(t_champ *champ, t_cor *cor)
+{
+  int	**tab;
+
+  tab = get_encode(cor->mem, champ->pc);
+  if (tab[0][0] == 1 && check_reg(tab[0][2]))
+    {
+      champ->carry = 1;
+      my_putchar(champ->reg[tab[0][2]] % 256, 1);
+    }
+  champ->pc += 6;
+  return (6);
+}
