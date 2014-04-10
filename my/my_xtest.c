4ad2f484284c52b9c07384694fb5e93f501fc3c6
fave_r <fave_r@epitech.eu>
2014-04-10 13:28:30 +0200

    test

--- my/my_xtest.c
+++ my/my_xtest.c
@@ -5,7 +5,7 @@
 ** Login   <thibaut.lopez@epitech.net>
 **
 ** Started on  Sun Feb 23 21:21:10 2014 Thibaut Lopez
-** Last update Mon Mar 24 12:14:55 2014 romaric
+** Last update Thu Apr 10 13:27:24 2014 romaric
 */
 
 #include "my.h"
@@ -42,7 +42,7 @@ ssize_t	xread(int fd, void *buf, size_t count)
   int	x;
 
   x = read(fd, buf, count);
-  if (x == -1)
+  if (x < 0)
     {
       my_putstr(ER_READ, 2);
       exit(EXIT_FAILURE);
